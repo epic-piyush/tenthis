32fbd3cc9b0e233b63fc417cb7d764d2f30f16f8
Piyush Pratap Singh <piyushpratapmewada@gmail.com>
2026-09-18 22:48:04 +0530

    Update Style for Light and Dark Theme

--- php/head.php
+++ php/head.php
@@ -10,11 +10,11 @@
   <meta name="description" content="Complete Task & Earn. Refer & Earn Money. Tenthis Web">
   <meta name="author" content="tenthisweb">
   <meta name="keywords" content="task, earn, money, tenthis, tenthisweb, freepaytmcash, paytm, cashback, cash, refer">
-  <link href="style_<?php echo $_COOKIE['theme']; ?>.css" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
   <script src="script.js"></script>
 </head>
 
-<body>
+<body class="<?php echo $_COOKIE['theme']; ?>">
   <div class="contact-wrapper">
   <div class="header">
     <header> <h2><?php echo $web_title; ?></h2>
